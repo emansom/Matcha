29eba92eaaf174147f34c887204fdc34a47cd106
Ewout van Mansom <ewout@freedom.nl>
2018-05-30 14:57:20 +0200

    Default badges for registered users

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -88,20 +88,20 @@ class RegisterController extends ControllerBase
             $user = new \Users();
             $user->username = $username;
             $user->password = $this->security->hash($password);
-            $user->motto = $this->config->newUser->defaultMotto;
-            $user->credits = $this->config->newUser->defaultCredits;
-            $user->tickets = $this->config->newUser->defaultTickets;
-            $user->film = $this->config->newUser->defaultFilm;
-            $user->rank = $this->config->newUser->defaultRank;
-            $user->console_motto = $this->config->newUser->defaultConsoleMotto;
+            $user->motto = $this->config->newUser->motto;
+            $user->credits = $this->config->newUser->credits;
+            $user->tickets = $this->config->newUser->tickets;
+            $user->film = $this->config->newUser->film;
+            $user->rank = $this->config->newUser->rank;
+            $user->console_motto = $this->config->newUser->consoleMotto;
             $user->last_online = 0;
             $user->sso_ticket = '';
             $user->pool_figure = '';
             $user->club_subscribed = 0;
             $user->club_expiration = 0;
-            $user->badge = '';
-            $user->badge_active = 0;
-            $user->allow_stalking = 1;
+            $user->badge = $this->config->newUser->badges[0];
+            $user->badge_active = sizeof($this->config->newUser->badges) > 0;
+            $user->allow_stalking = $this->config->newUser->allowStalking;
 
             if ($this->session->has('register-gender')) {
                 $user->sex = $this->session->get('register-gender');
@@ -122,6 +122,22 @@ class RegisterController extends ControllerBase
                 $this->response->setJsonContent($user->getMessages());
                 $this->response->send();
             } else {
+                // Create badges for user
+                $badges = [];
+                $i = 0;
+
+                foreach ($this->config->newUser->badges as $badge) {
+                    $badges[$i] = new UsersBadges();
+                    $badges[$i]->user_id = $user->id;
+                    $badges[$i]->badge = $badge;
+                    $badges[$i]->create();
+                    $i++;
+                }
+
+                // Let ORM know about badges
+                $user->badges = $badges;
+                $user->update();
+
                 // Regenerate session id to protect from session hijacking
                 $this->session->regenerateId(true);
                 $this->session->set("user_id", $user->id);
